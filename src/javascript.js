68379f9e0981d2f933af943c67ac89c732388fa9
HMKL <catalin.bac@gmail.com>
2022-05-03 21:18:11 +0300

    some changes

--- src/javascript.js
+++ src/javascript.js
@@ -1,8 +1,14 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable max-len */
 /* eslint-disable no-console */
-/* eslint-disable linebreak-style */
-const addClass = document.getElementById('navleft').querySelectorAll('div');
+const getDescription = document.querySelectorAll('.container > div >.row > div ');
+getDescription[0].classList.add('col-md-4');
+getDescription[1].classList.add('col-md-8');
+const getRows = document.querySelectorAll('.row');
+const getContainer = document.querySelectorAll('.container-fluid');
+getContainer[1].classList.add('_1');
 
+const addClass = document.getElementById('navleft').querySelectorAll('div');
 for (let i = 0; i < addClass.length; i += 1) {
   if (addClass.length) {
     addClass[i].addEventListener('mouseenter', () => {
@@ -14,7 +20,6 @@ for (let i = 0; i < addClass.length; i += 1) {
 }
 
 const removeClass = document.getElementById('navleft').querySelectorAll('div');
-
 for (let a = 0; a < removeClass.length; a += 1) {
   if (removeClass.length) {
     removeClass[a].addEventListener('mouseout', () => {
@@ -25,6 +30,44 @@ for (let a = 0; a < removeClass.length; a += 1) {
   }
 }
 
-const getContainer = document.querySelectorAll('.container-fluid');
-// getContainer[1].style.margin = '0px 10px 0px 10px';
-getContainer[1].classList.add('_1');
+let count = 0;
+const collapseAdd = document.getElementById('testBtn');
+collapseAdd.addEventListener('click', () => {
+  count += 1;
+  if (count === 1) {
+    document.querySelector('#navbarCollapse').classList.add('inherit');
+    document.querySelector('.container').style.display = 'none';
+    document.querySelector('#testBtn > span').innerHTML = '<i class="fa-solid fa-grip-lines-vertical"></i>';
+  } else {
+    document.querySelector('#navbarCollapse').classList.remove('inherit');
+    document.querySelector('.container').style.display = 'block';
+    document.querySelector('#testBtn > span').innerHTML = '<i class="fa-solid fa-bars"></i>';
+    count -= 2;
+  }
+});
+
+const width = window.matchMedia('(max-width:769px)');
+const allDivs = document.querySelectorAll('.container > div');
+const allA = document.querySelectorAll('#navleft > div > a');
+allA.forEach((square) => {
+  square.classList.add('text-center');
+});
+allDivs.forEach((div) => {
+  div.classList.add('col-12');
+});
+function screen(a) {
+  if (a.matches) {
+    for (let i = 0; i < allDivs.length; i += 1) {
+      allA[i].classList.add('text-center');
+      document.querySelector('#navbarCollapse').classList.add('inherit');
+      getRows[0].classList.add('flex-column-reverse', 'flex-md-row');
+    }
+  } else {
+    for (let y = 0; y < allDivs.length; y += 1) {
+      allA[y].classList.remove('text-center');
+      document.querySelector('#navbarCollapse').classList.remove('inherit');
+      getRows[0].classList.remove('flex-column-reverse', 'flex-md-row');
+    }
+  }
+}
+width.addEventListener('change', screen);
